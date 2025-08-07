a260a06a29bbeb314638fad2f2effbf412857ae0
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2025-08-07 18:57:17 +0100

    Refactor HTML structure, enhance CSS styles, and initialize JavaScript for IP address tracking functionality

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const ipifyUrl = "https://geo.ipify.org/api/v2/country?apiKey=at_ejhYewfqwtYccU94h8y3BgoFbfhs9&ipAddress="
+let ipInput = document.getElementById("ip-input");
 let map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
