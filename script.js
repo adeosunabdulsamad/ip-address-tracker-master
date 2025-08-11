5e8aa067d90d429fd7237f8e71ed67b613da594e
Adeosun Abdulsamad <adeosunabdulsamad25@gmail.com>
2025-08-11 20:23:59 +0100

    Enhance UI for IP address tracking: add autocomplete to input, improve layout and styling, and update displayed information structure

--- script.js
+++ script.js
@@ -3,10 +3,16 @@ let ipErrorMessage = document.getElementById("ip-error");
 let ipAddress = document.getElementById("ip-Address");
 let ipLocation = document.getElementById("ip-Location");
 let ipTimezone = document.getElementById("ip-Timezone");
-let ipPostalCode = document.getElementById("ip-postalCode");
 let ipISP = document.getElementById("ip_ISP");
 const ipRegex = /^(?:(?:\d{1,3}\.){3}\d{1,3}|(?:[A-Fa-f0-9]{1,4}:){7}[A-Fa-f0-9]{1,4})$/;
 let currentIpifyUrl;
+let locationIcon = L.icon({
+    iconUrl: './images/icon-location.svg',
+
+    iconSize:     [37, 65], // size of the icon
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
 let map = L.map('map').setView([51.505, -0.09], 13);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
@@ -31,12 +37,11 @@ function myFunction() {
         .then(data => {
             console.log('Data Received:', data);
             ipAddress.textContent = data.ip || ipInput.value;
-            ipLocation.textContent = data.location ? `${data.location.country}, ${data.location.region}` : 'N/A';
+            ipLocation.textContent = data.location ? `${data.location.country}, ${data.location.region} ${data.location.postalCode}` : 'N/A';
             ipTimezone.textContent = data.location ? `UTC ${data.location.timezone}` : 'N/A';
             ipISP.textContent = data.isp || 'N/A';
-            ipPostalCode.textContent = data.location ? data.location.postalCode : 'N/A';
             map.setView([data.location.lat, data.location.lng], 13);
-            let marker = L.marker([data.location.lat, data.location.lng]).addTo(map);
+            let marker = L.marker([data.location.lat, data.location.lng], {icon: locationIcon}).addTo(map);
         })
         .catch(error => {
             console.log('There was an error', error);
